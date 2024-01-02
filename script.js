f9c71cacb4526bead3f38967de96100bede0662a
wlfbbz <44901408+wlfbbz@users.noreply.github.com>
2024-01-02 04:51:15 +0000

    final

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-let slideIndex = 0;
-showSlides();
+    setTimeout(function() {
+        window.location.href = 'signup.html';
+    }, 3500);
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("image-box");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-        slideIndex++;
-    if (slideIndex > slides.length) {
-        // Redirect to a new page when the slideshow ends
-        window.location.href = "signup.html"; // Change this URL to your desired location
-        return; // Stop the function from continuing
-    }    
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
